e1ea0a31f3bca3d937959763ecfb701b863ef692
WilbertVPma <118822666+WilbertVPma@users.noreply.github.com>
2023-02-19 13:16:45 -0500

    Add files via upload

--- Practica_SQLAvanzado2.sql
+++ Practica_SQLAvanzado2.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE TABLE keepcoding.ivr_summary AS(
-WITH PRACTICA_P2 AS(
+WITH CALCULO_HORAS AS(
 SELECT calls_phone_number AS phone_number
       ,calls_start_date AS start_date
       ,IF(TIMESTAMP_DIFF(calls_start_date, LAG(calls_start_date) OVER(ORDER BY calls_phone_number), HOUR) < 24
@@ -35,19 +35,19 @@ SELECT calls_phone_number AS phone_number
        AND d2.step_description_error = 'NULL', 1, 0)) AS info_by_phone_lg
       ,MAX(IF(d2.step_name = 'CUSTOMERINFOBYDNI.TX' 
        AND d2.step_description_error = 'NULL', 1, 0)) AS info_by_dni_lg
-      ,PRACTICA_P2.repeated_phone_24H
-      ,PRACTICA_P2.cause_recall_phone_24H
+      ,CALCULO_HORAS.repeated_phone_24H
+      ,CALCULO_HORAS.cause_recall_phone_24H
   FROM keepcoding.ivr_detail d1
     JOIN keepcoding.ivr_detail d2
     ON d1.calls_ivr_id = d2.calls_ivr_id
-    JOIN PRACTICA_P2
+    JOIN CALCULO_HORAS
     ON phone_number = d1.calls_phone_number AND start_date = d1.calls_start_date
     GROUP BY d1.calls_ivr_id, d1.calls_phone_number, d1.calls_ivr_result, d1.calls_vdn_label, d1.calls_start_date
             ,d1.calls_end_date, d1.calls_total_duration, d1.calls_customer_segment, d1.calls_ivr_language
             ,d1.calls_steps_module, d1.calls_module_aggregation, d1.document_type, d1.document_identification, d1.customer_phone
             ,d1.billing_account_id, d1.module_name, d1.step_name, d1.step_description_error, d1.calls_ivr_id
-            ,d2.calls_start_date, d2.calls_phone_number, d2.calls_ivr_id, phone_number,PRACTICA_P2.repeated_phone_24H
-            ,PRACTICA_P2.cause_recall_phone_24H
+            ,d2.calls_start_date, d2.calls_phone_number, d2.calls_ivr_id, phone_number,CALCULO_HORAS.repeated_phone_24H
+            ,CALCULO_HORAS.cause_recall_phone_24H
         QUALIFY ROW_NUMBER() OVER(PARTITION BY d1.calls_ivr_id ORDER BY d1.document_type NULLS LAST,
         d1.document_identification NULLS LAST, d1.customer_phone NULLS LAST, d1.billing_account_id NULLS LAST) = 1
       ORDER BY phone_number, start_date ASC)
